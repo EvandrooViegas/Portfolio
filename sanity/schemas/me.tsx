7034827a1b89d7b63d5c9ac2a03382975a2ea55e
Evandro Viegas <evandrooviegas@gmail.com>
2023-07-24 10:51:14 +0100

    Added Testimonials field on sanity schema

--- sanity/schemas/me.tsx
+++ sanity/schemas/me.tsx
@@ -122,5 +122,42 @@ export const me = defineType({
         return true
       })
     }),
+    defineField({
+      title: "Testimonials",
+      name: "testimonials",
+      type: "array",
+      of: [
+        defineArrayMember({
+          title: "Testimonial",
+          name: "testimonial",
+          type: "document",
+          fields: [
+            defineField({
+              title: "Name",
+              name: "name",
+              type: "string",
+              validation: r => r.required()
+            }),
+            defineField({
+              title: "Message",
+              name: "message",
+              type: "string",
+              validation: r => r.required()
+            }),
+            defineField({
+              title: "Avatar",
+              name: "avatar",
+              type: "image",
+              validation: r => r.required()
+            }),
+          ]
+        }),
+      ],
+      validation: r => r.required().custom((info: any) => {
+        if(info.length !== 3) return 'Testimonials size shall be 3'
+        return true
+      })
+    }),
+
   ],
 });
